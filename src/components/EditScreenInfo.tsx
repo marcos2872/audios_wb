a08b164bc78d6044f735dcd09f603de54b06a246
Marcos Brito <njsu5dev@hotmail.com>
2023-03-02 17:36:29 -0300

    instalando o eslint

--- src/components/EditScreenInfo.tsx
+++ src/components/EditScreenInfo.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from 'react-native'
 
 import Colors from '../constants/Colors';
 import { ExternalLink } from './ExternalLink';
