f4929cf8c123eafb71316f8c204ae15b5ecd5847
Marcos Brito <njsu5dev@hotmail.com>
2023-03-03 18:52:36 -0300

    criando a tela de player

--- src/screens/Player/index.tsx
+++ src/screens/Player/index.tsx
@@ -1,14 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, SafeAreaView } from 'react-native'
 import { useRoute } from '@react-navigation/native';
+import { stylesPlayer } from './styles.Player';
+import data from '../../mock/data';
+import { useEffect, useState } from 'react';
+import { IData } from '../../interfaces/IDataApi';
 
 const Player = () => {
   const { params: { id } } = useRoute() as { params: { id: string } };
-  console.log(id);
+  const [playerData, setPlayerData] = useState<IData>()
+  
+  useEffect(() => {
+    const info = data.find((curr) => curr.id === id)
+    setPlayerData(info)
+  }, [])
+  console.log(playerData)
   
   return (
-    <View>
+    <SafeAreaView style={stylesPlayer.main}>
       <Text>Player</Text>
-    </View>
+    </SafeAreaView>
   )
 }
 
---
+++ src/screens/Player/styles.Player.ts
@@ -0,0 +1,9 @@
+import { StyleSheet } from "react-native";
+
+export const stylesPlayer = StyleSheet.create({
+  main: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
